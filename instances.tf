fbe732290f3ba20455eb0e58db85b7504ef4d719
Ludovic Logiou <ludovic.logiou@objectif-libre.com>
2020-01-25 17:15:33 +0100

    [Terraform] Fix variable interpolation

--- instances.tf
+++ instances.tf
@@ -1,14 +1,14 @@
 resource "scaleway_instance_server" "instance" {
-  count = "${var.vm_number}"
+  count = var.vm_number
 
-  type               = "${var.vm_type}"
-  image              = "${var.vm_image}"
+  type               = var.vm_type
+  image              = var.vm_image
   name               = "${var.vm_name}-${count.index}"
-  tags               = "${var.vm_tags}"
-  security_group_id  = "${scaleway_instance_security_group.secgroup.id}"
-  placement_group_id = "${var.vm_pg}"
-  enable_dynamic_ip = "${var.vm_disable_dynamic_ip}"
-  ip_id              = "${var.vm_public_ip}"
+  tags               = var.vm_tags
+  security_group_id  = scaleway_instance_security_group.secgroup.id
+  placement_group_id = var.vm_pg
+  enable_dynamic_ip  = var.vm_disable_dynamic_ip
+  ip_id              = var.vm_public_ip
 
   dynamic "user_data" {
     iterator = data
